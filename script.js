d71b2dc05f3d213e931c4e1bb0cd38f7f38297cf
fartylocal <111maksimus7@gmail.com>
2023-11-11 16:00:10 +0100

    1

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ function calculateAge() {
     const ageInput = document.getElementById('ageInput');
     const age = parseInt(ageInput.value);
     const nextYearAge = age + 1;
-
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `You will be ${nextYearAge} years old in 1 year.`;
+    if (age < 110) {
+        resultElement.textContent = `You will be ${nextYearAge} years old in a year.`;
+    };
+    if (age > 119) {
+        resultElement.textContent = "How the hell are u still not dead ummm";
+    };
 }
+
+document.getElementById('resetbtn').addEventListener("click", function() {
+    location.reload()
+})
