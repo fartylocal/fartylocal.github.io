40b868d138838c79b14a2f77b40ae5be22df2e76
fartylocal <111maksimus7@gmail.com>
2024-01-06 17:14:29 +0100

    dfsdf

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-var textarea = document.querySelector(".country");
-var btn = document.querySelector("button");
-var container = document.querySelector(".container");
+var input = document.getElementById("a")
+var btn = document.getElementById("b")
 
-var JiafeiFunction = Jiafei;
-
-btn.addEventListener('click', JiafeiFunction); 
-
-function Jiafei() {
-  console.log("Millie bobby brown is cvmming");
-  console.log(textarea.value);
-  if (textarea.value !== "Floptropica") {
-    container.innerHTML =
-    `<p id="result" style="border: 3px dotted red; height: 300px; margin: 0px auto 18px auto; line-height: 300px; font-size: 30px;">
-      Wrong! Setting your house on fire...
-    </p>`;
-    var audio = new Audio("fire.mp3");
-    audio.play();
-  }
-  if (textarea.value === "Floptropica") {
-    container.innerHTML =
-    `<p id="result" style="border: 3px dotted green; height: 300px;text-align: left; vertical-align: middle;margin: 0px auto 18px auto; padding: 5px;font-size: 30px;">
-      Greetings. <br> <br> Your answer was correct! Queen Jiafei is inviting you to her coochielicious haunted house! Bring all your family and friends! <br><br> Best regards,<br> Jiafei
-    </p>`;
-    var audio = new Audio("correct.mp3");
-    audio.play();
-  }
-
-}
+btn.addEventListener('click', () => {
+var text = input.value
+var main = document.querySelector(".main")
+main.innerHTML = 
+`
+        <p>Dear, ${text}. You have diagnosed with diabetes, scoliosis, major brain injury. Diagnosed with Fartylocals diagnosis tool.</p>
+        <p>I touched ${text} wool and got the lily pad disease... Never again...</p>
+        <p>Dear ${text}, your Roblox version is outdated. Please install a newer version of Roblox.</p>
+        <p>🦢🎀Dear ${text}, i will sell you on starpets for 6 cents 🎀🦢</p>
+`
+})
