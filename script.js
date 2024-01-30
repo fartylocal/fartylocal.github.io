134b7af8ec8f976f1db07b2f524c4532d6d942c3
fartylocal <111maksimus7@gmail.com>
2024-01-30 13:13:37 +0100

    newgenerator

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 var input = document.getElementById("a")
 var btn = document.getElementById("b")
-
+function insertc(inputString, charToInsert) {
+        const charsArray = inputString.split('');
+        const resultString = charsArray.join(charToInsert) + charToInsert;
+        return resultString;
+      }
 btn.addEventListener('click', () => {
 var text = input.value
 var main = document.querySelector(".main")
-main.innerHTML = 
-`
-        <p>Dear, ${text}. You have diagnosed with diabetes, scoliosis, major brain injury. Diagnosed with Fartylocals diagnosis tool.</p>
-        <p>I touched ${text} wool and got the lily pad disease... Never again...</p>
-        <p>Dear ${text}, your Roblox version is outdated. Please install a newer version of Roblox.</p>
-        <p>🦢🎀Dear ${text}, i will sell you on starpets for 6 cents 🎀🦢</p>
-`
+main.innerHTML = insertc(text,"︡")
 })
